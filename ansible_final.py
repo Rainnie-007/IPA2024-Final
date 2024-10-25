c071b70c26cd5826b82f43675d178fb0c6c30024
Rainnie-007 <109953295+Rainnie-007@users.noreply.github.com>
2024-10-25 08:26:13 +0000

    Add files via upload

--- ansible_final.py
+++ ansible_final.py
@@ -1,11 +1,20 @@
 import subprocess
 
-def showrun():
-    # read https://www.datacamp.com/tutorial/python-subprocess to learn more about subprocess
-    command = ['<!!!REPLACEME with ansible command to run playbook!!!>', '<!!!REPLACEME with playbook yaml file!!!>']
+def showrun(student_id, router_name):
+    command = ['ansible-playbook', '/home/devasc/Desktop/Final/final193/IPA2024-Final/playbook.yaml']
     result = subprocess.run(command, capture_output=True, text=True)
-    result = result.stdout
-    if 'ok=2' in result:
-        return <!!!REPLACEME!!!>
+    
+    
+    print("Standard Output:\n", result.stdout)
+    print("Standard Error:\n", result.stderr)
+    
+    
+    if result.returncode == 0:  # Ansible success returns 0
+        return f'show_run_{student_id}_{router_name}.txt'
     else:
-        return '<!!!REPLACEME!!!>
+        return 'Error: Ansible'
+
+student_id = '65070193'
+router_name = 'IPA2024-Pod1-4'
+output_file = showrun(student_id, router_name)
+print(output_file)
